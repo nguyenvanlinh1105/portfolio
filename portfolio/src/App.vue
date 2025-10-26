8a016be952a99d2c9fcc436649b8f7898294f466
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2025-10-26 16:33:49 +0700

    update view page

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header/Header.vue'
-// import About from './components/About/About.vue';
+import About from './components/About/About.vue';
 import Project from './components/Project/Project.vue';
 // import Hero from './components/Hero/Hero.vue';
 </script>
 
 <template>
   <Header></Header>
-  <!-- <About/> -->
+  <About/>
   <Project/>
   <!-- <Card/> -->
   <!-- <Card/> -->
